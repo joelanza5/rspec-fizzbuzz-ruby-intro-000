ded43d4b0695d0eb0af16269a788897e15f16b7d
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-25 15:26:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
 if int % 3 == 0
-  puts "Fizz"
+  print "Fizz"
 elsif int % 5 == 0
-  puts "Buzz"
+  print "Buzz"
 elsif int % (3 && 5) == 0
-  puts "Fizzbuzz"
+  print "Fizzbuzz"
 else
   puts "nil"
 end
