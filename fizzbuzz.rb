b3c45baae0d85d7ec8830df88ce97678de67fce0
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-25 15:06:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(int)
+if int % 3 == 0
+  puts "Fizz"
+elsif int % 5 == 0
+  puts "Buzz"
+elsif int % (3 && 5)
+  puts "Fizzbuzz"
+else
+  puts "nil"
+end
+end
+
+fizz_3 = fizzbuzz(3)
+fizz_4 = fizzbuzz(4)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
